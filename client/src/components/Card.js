a7c3327ae1ec5e14bb1bcb077d838f3f0bd562cd
Aditya sharma <adityasharma10102000@gmail.com>
2024-03-19 20:43:40 +0530

    add animation in galleryCard and card section

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,16 +1,12 @@
-export default function Card({ image, price, size, description }) {
+export default function Card({ image, description }) {
   return (
-    <div className="max-w-xs overflow-hidden rounded shadow-lg ">
-      <img className="w-full" src={image} alt="Product" />
-      <div className="px-6 py-4">
-        <div className="mb-2 text-xl font-bold">{price}</div>
-        <p className="text-base text-gray-700">{description}</p>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{size}</span>
-        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
-          Buy
-        </button>
+    <div className="max-w-xs overflow-hidden rounded-lg shadow-lg">
+      <div className="transition-transform duration-300 transform hover:scale-110">
+        <img className="w-full" src={image} alt="Product" />
+        <div className="flex flex-wrap px-4 py-2 text-gray-800 bg-white">
+          <p className="font-bold text-md">{description}</p>
+          <div className="font-bold text-md">Contact: 7974507514</div>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/GalleryCard.js
+++ client/src/pages/GalleryCard.js
@@ -1,6 +1,6 @@
 export default function GalleryCard({ imageUrl, itemName, price, description, onClick,size }) {
     return (
-        <div className="p-4 bg-gray-200 rounded-lg" onClick={onClick}>
+        <div className="p-4 transition-transform duration-300 transform bg-gray-200 rounded-lg hover:scale-110" onClick={onClick}>
             <img src={imageUrl} alt={itemName} className="w-full h-auto rounded-lg cursor-pointer" loading="lazy" />
             <div className="mt-2">
                 <p className="text-lg font-semibold">{itemName}</p>
