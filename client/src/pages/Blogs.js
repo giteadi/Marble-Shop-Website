501832682e1973069198832c594bf3624b2a6439
Aditya sharma <adityasharma10102000@gmail.com>
2024-04-24 21:21:05 +0530

    blogs section added

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -2,10 +2,48 @@ import React from 'react';
 
 export default function Blogs() {
     return (
-        <div className="container px-4 py-8 mx-auto">
-            <h1 className="mb-4 text-3xl font-bold">Welcome to Our Blog</h1>
-            <p className="mb-4 text-lg">Stay tuned for the latest updates, news, and stories about our marble products.</p>
-            {/* Add blog posts here */}
+        <div>
+           
+           <div className="container box-border flex flex-wrap items-center justify-center gap-4 mt-4">
+           <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/RhOisqrYpCw"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/O0k1HPcZnrg"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/XtsUcqa0CUY"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/exxj2oliHTo"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+           </div>
+             
+             
         </div>
     )
 }
